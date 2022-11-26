5645f8f873f986a511ebb4b3ff637b6dd800d185
konung-yaropolk <yaropolk1995@gmail.com>
2022-11-26 03:16:18 -0600

    new file:   clang_int.elf 	new file:   gcc_int.elf 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,29 @@
-#! /bin/bash
+#! /bin/sh
 
 g++ int.cpp -o gcc_int.elf
 clang++ int.cpp -o clang_int.elf
 
-time gcc_int.elf
-time clang_int.elf
+echo "GCC compiler:"
+echo ""
+
+echo "Integers:"
+time ./gcc_int.elf
+
+
+echo ""
+echo ""
+echo ""
+echo "Clang compiler:"
+echo ""
+echo "Integers:"
+time ./clang_int.elf
+
+
+
+
+echo ""
+echo ""
+echo ""
+echo "Done! Anykey to exit..."
 
 read
