6d3f9f4d8476ffbf113c02480a5cfebfef055dcb
konung-yaropolk <yaropolk1995@gmail.com>
2023-02-06 18:14:17 -0600

    Update float.cpp

--- float.cpp
+++ float.cpp
@@ -2,8 +2,9 @@
 
 int main() {
     int n=1;
+    int repeats=100000000;
     long double pi=1;
-    while (n<100000000) {
+    while (n<repeats) {
         pi=pi * 2*n/(2*n-1) * 2*n/(2*n+1);
         n++;
     }
